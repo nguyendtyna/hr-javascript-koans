d2457731f2c7d8f6dee4577f6b543e2c422d01eb
Tyna Nguyen <ngtyna@gmail.com>
2020-08-26 14:47:56 -0500

    Correct the first AboutObjects koans to show objects are collections of properties

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -4,16 +4,19 @@ describe('About Objects', function() {
     var meglomaniac;
 
     beforeEach(function() {
-      meglomaniac = { mastermind: 'Joker', henchwoman: 'Harley' };
+      meglomaniac = {
+        mastermind: 'Joker',
+        henchwoman: 'Harley'
+      };
     });
 
     it('should confirm objects are collections of properties', function() {
-      expect(meglomaniac.mastermind).toBe(FILL_ME_IN);
+      expect(meglomaniac.mastermind).toBe('Joker');
     });
 
     it('should confirm that properties are case sensitive', function() {
-      expect(meglomaniac.henchwoman).toBe(FILL_ME_IN);
-      expect(meglomaniac.henchWoman).toBe(FILL_ME_IN);
+      expect(meglomaniac.henchwoman).toBe('Harley');
+      expect(meglomaniac.henchWoman).toBe(undefined);
     });
   });
 
